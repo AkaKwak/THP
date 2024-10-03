4d060d21296cef644f5dcff7b0eeb92e1293fa43
AkaKwak <christophe.alaterre@gmail.com>
2024-10-03 21:44:10 +0200

    sorted list

--- week2/week2day4/Exercice_ruby_day4/lib/00_alphabet_sort.rb
+++ week2/week2day4/Exercice_ruby_day4/lib/00_alphabet_sort.rb
@@ -1,11 +1,11 @@
 require_relative("./00_journalists.rb")
 
 def alphabet_order
-  sorted_list = $list.sort |sorted_list|
+  sorted_list = $list.sort
+# puts "#{sorted_list}"
 end
 alphabet_order
 
+
 # do |sorting|sorting.sort 
-# 
-# puts "#{sorted_list}"
 #Appelez $list / map / puis sort  / dans un array / que j'affiche pour vérifier l'ordre
